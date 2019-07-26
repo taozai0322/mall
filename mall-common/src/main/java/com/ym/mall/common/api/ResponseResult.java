1f67ca5789e4da322393c4f15983cb46ac218d0a
matao <tao.ma6@pactera.com>
2019-07-26 10:00:13 +0800

    添加公共模块mall-common

--- mall-common/src/main/java/com/ym/mall/common/api/ResponseResult.java
+++ mall-common/src/main/java/com/ym/mall/common/api/ResponseResult.java
@@ -38,7 +38,48 @@ public class ResponseResult<T> {
      * @param data
      * @return
      */
-    public ResponseResult<T> success(T data){
+    public static <T> ResponseResult<T> success(T data){
         return new ResponseResult(ResultCode.SUCCESS.getCode(),ResultCode.SUCCESS.getMsg(),data);
+    }
+
+    /**
+     * 成功返回有数据
+     * @param data   获取的数据
+     * @param msg    提示信息
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseResult<T> success(T data,String msg){
+        return new ResponseResult(ResultCode.SUCCESS.getCode(),msg,data);
+    }
+
+    /**
+     * 失败返回结果
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseResult<T> fail(){
+        return new ResponseResult(ResultCode.FAIL.getCode(),ResultCode.FAIL.getMsg(),null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param msg   提示错误信息
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseResult<T> fail(String msg){
+        return new ResponseResult(ResultCode.FAIL.getCode(),msg,null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param code  提示错误码
+     * @param msg   错误提示信息
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseResult<T> fail(String code,String msg){
+        return new ResponseResult(code,msg,null);
     }
 }
--- mall-common/src/main/java/com/ym/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/ym/mall/common/api/ResultCode.java
@@ -7,8 +7,10 @@ package com.ym.mall.common.api;
 public enum ResultCode implements ErrorCode{
 
     SUCCESS("200","操作成功"),
-    FAIL("500","操作失败");
-
+    FAIL("500","操作失败"),
+    VALIDATE_PARAMS_FAILED("404", "参数检验失败"),
+    UNAUTHORIZED("401", "暂未登录或token已经过期"),
+    FORBIDDEN("403", "没有相关权限");
 
 
     private String code;
